d3eb2300b68402fdbd5d0587c7c56a23330f20c0
BenjaGreen012 <benjaarmijo02@gmail.com>
2022-11-30 19:37:34 +0100

    Acabado lo del mes soguiente y mes anterior

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 "use strict";
 let diasSetmana = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+let mesesDelAnyo = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosot', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+let escribirAnyo = document.getElementById('anyo');
+let escribirMes = document.getElementById('mes');
 let generarDias = document.getElementById('dias');
 let numDias = document.getElementById('numsDias');
 let cosasDate = new Date();
 let anyo = cosasDate.getFullYear();
-// let mes = cosasDate.getMonth();
-let mes = 4;
+let mes = cosasDate.getMonth();
+// let mes = 4;
+let anyoActual=cosasDate.getFullYear();
 let ultimo = new Date(anyo, mes + 1, 0);
 let primero = new Date(anyo, mes, 1);
 const DIAS = 7;
@@ -21,7 +25,7 @@ function generarDiasSetmana() {
     }
 }
 function generarMesAnterior() {
-    if (mes>=0) {
+    if (mes > 0) {
         numDias.innerHTML = "";
         mes = mes - 1;
         console.log(mes);
@@ -30,12 +34,24 @@ function generarMesAnterior() {
         primeraSetmana = primero.getDay();
         cont = 1;
         generarNumerosCaledario();
-    }else{
-        alert("Se acabo el año");
+        escribirMes.innerHTML = mesesDelAnyo[mes];
+    } else {
+        mes = 11;
+        anyo = anyo - 1;
+        numDias.innerHTML = "";
+        console.log(mes);
+        ultimo = new Date(anyo, mes + 1, 0);
+        primero = new Date(anyo, mes, 1);
+        primeraSetmana = primero.getDay();
+        cont = 1;
+        generarNumerosCaledario();
+        escribirMes.innerHTML = mesesDelAnyo[mes];
+        escribirAnyo.innerHTML = anyo;
+        console.log(anyo);
     }
 }
 function generarMesSiguiente() {
-    if (mes<11) {
+    if (mes < 11) {
         numDias.innerHTML = "";
         mes = mes + 1;
         console.log(mes);
@@ -44,11 +60,24 @@ function generarMesSiguiente() {
         primeraSetmana = primero.getDay();
         cont = 1;
         generarNumerosCaledario();
-    }else{
-        alert("Se acabo el año");
+        escribirMes.innerHTML = mesesDelAnyo[mes];
+    } else {
+        numDias.innerHTML = "";
+        mes = 0;
+        anyo=anyo+1;
+        console.log(mes);
+        ultimo = new Date(anyo, mes + 1, 0);
+        primero = new Date(anyo, mes, 1);
+        primeraSetmana = primero.getDay();
+        cont = 1;
+        generarNumerosCaledario();
+        escribirMes.innerHTML = mesesDelAnyo[mes];
+        escribirAnyo.innerHTML=anyo;
     }
 }
 function generarNumerosCaledario() {
+    escribirMes.innerHTML = mesesDelAnyo[mes];
+    escribirAnyo.innerHTML = anyo;
     for (let i = 0; i < LINEAS; i++) {
         for (let j = 0; j < DIAS; j++) {
             if (primeraSetmana > 0) {
